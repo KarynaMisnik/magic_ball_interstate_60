cd24e8a40a224eaac7ed04bd6bb37daeb711d5a9
KarynaMisnik <96831988+KarynaMisnik@users.noreply.github.com>
2022-09-14 18:14:32 +0300

    Add files via upload

---
+++ qenerator.sql
@@ -0,0 +1,26 @@
+create table qenerator (
+id int(50),
+text varchar(150),
+primary key (id)
+);
+
+insert into qenerator values ('1', 'It is certain');
+insert into qenerator values ('2', 'It is decidedly so');
+insert into qenerator values ('3', 'Without a doubt');
+insert into qenerator values ('4', 'Yes definitely');
+insert into qenerator values ('5', 'You may rely on it');
+insert into qenerator values ('6', 'As I see it, yes');
+insert into qenerator values ('7', 'Most likely');
+insert into qenerator values ('8', 'Outlook good');
+insert into qenerator values ('9', 'Yes');
+insert into qenerator values ('10', 'Signs point to yes');
+insert into qenerator values ('11', 'Reply hazy, try again');
+insert into qenerator values ('12', 'Ask again later');
+insert into qenerator values ('13', 'Better not tell you now');
+insert into qenerator values ('14', 'Cannot predict now');
+insert into qenerator values ('15', 'Concentrate and ask again');
+insert into qenerator values ('16', 'Do not count on it');
+insert into qenerator values ('17', 'My reply is no');
+insert into qenerator values ('18', 'My sources say no');
+insert into qenerator values ('19', 'Outlook not so good');
+insert into qenerator values ('20', 'Very doubtful');
